e0a30e082f5202564351c18d2a1f0e4b4e44f4fb
Resara <rmwthorne@googlemail.com>
2017-08-08 15:55:11 +0100

    Fixed issue preventing split_position in NAMESPACE

--- R/import.R
+++ R/import.R
@@ -26,9 +26,8 @@ strip_suffix <- function(x) gsub(pattern = "_1|_2|_3", replacement = "", x = x)
 #'
 #' @param x A character object in the format chr:start-stop
 #' @return A 3-column data frame form the values in \code{x}.
-#' @examples
-#' split_position
 #'
+
 split_position <- function(chr) {
   # Regex-split a Chr:Start-Stop string
   if (!grepl(":", chr) | !grepl("-", chr)) {
@@ -44,40 +43,40 @@ split_position <- function(chr) {
 
 
 
-tidy_gfc <- function(gene.expn, window, smooth = 10, path = "data") {
-  message("Extracting data for ", gene.expn, "...")
-
-  # Load and merge the dataframes
-  gene.files <- list.files(path = path, pattern = gene.expn, full.names = T)
-  # message("Files matching pattern: \n - ", paste0(gene.files, collapse = "\n - "))
-  r_an.df <- read.table(gene.files[grepl(pattern = "R_analysis", x = gene.files)])
-  r_an.df <- r_an.df %>% mutate("Fragment" = rownames(r_an.df))
-  deseq.df <- read.table(gene.files[grepl(pattern = "DESeq_analysis", x = gene.files)])
-  deseq.df <- deseq.df %>% mutate("Fragment" = rownames(deseq.df))
-  df <- full_join(x = r_an.df, y = deseq.df, by = c("Fragment"))
-  df <- df %>% separate(Fragment, c("Chr","Start","Stop"), sep=":|-") %>%
-    mutate(Start = strip_comma(Start), Stop = strip_comma(Stop)) %>%
-    mutate(Position = (Start+Stop)/2 )
-
-  # Filter for the datapoints in the window
-  window <- split_position(window) %>% as.list()
-  df <- df %>% filter_(~Chr == window[[1]],
-                       ~Position > window[[2]],
-                       ~Position < window[[3]])
-
-  df <- df %>%
-    mutate_(Locus = ~gene.expn) %>%
-    mutate_(S.condA_mean = ~rollmean(condA_mean, smooth, fill = "extend")) %>%
-    mutate_(S.condB_mean = ~rollmean(condB_mean, smooth, fill = "extend")) %>%
-    mutate_(S.diff_mean = ~rollmean(dif_mean, smooth, fill = "extend")) %>%
-    mutate_(S.condA_SD = ~rollmean(condA_SD, smooth, fill = "extend")) %>%
-    mutate_(S.condB_SD = ~rollmean(condB_SD, smooth, fill = "extend"))
-
-  # Tidy
-  df.tidy <- df %>% gather_(key_col = "Condition", value_col = "Normalised.Mean",
-                            gather_cols = c("condA_mean", "condB_mean"))
-  df.tidy <- df %>% gather_(key_col = "S.Condition", value_col = "S.Normalised.Mean",
-                            gather_cols = c("S.condA_mean", "S.condB_mean"))
-  df.tidy
-}
+# tidy_gfc <- function(gene.expn, window, smooth = 10, path = "data") {
+#   message("Extracting data for ", gene.expn, "...")
+#
+#   # Load and merge the dataframes
+#   gene.files <- list.files(path = path, pattern = gene.expn, full.names = T)
+#   # message("Files matching pattern: \n - ", paste0(gene.files, collapse = "\n - "))
+#   r_an.df <- read.table(gene.files[grepl(pattern = "R_analysis", x = gene.files)])
+#   r_an.df <- r_an.df %>% mutate("Fragment" = rownames(r_an.df))
+#   deseq.df <- read.table(gene.files[grepl(pattern = "DESeq_analysis", x = gene.files)])
+#   deseq.df <- deseq.df %>% mutate("Fragment" = rownames(deseq.df))
+#   df <- full_join(x = r_an.df, y = deseq.df, by = c("Fragment"))
+#   df <- df %>% separate(Fragment, c("Chr","Start","Stop"), sep=":|-") %>%
+#     mutate(Start = strip_comma(Start), Stop = strip_comma(Stop)) %>%
+#     mutate(Position = (Start+Stop)/2 )
+#
+#   # Filter for the datapoints in the window
+#   window <- split_position(window) %>% as.list()
+#   df <- df %>% filter_(~Chr == window[[1]],
+#                        ~Position > window[[2]],
+#                        ~Position < window[[3]])
+#
+#   df <- df %>%
+#     mutate_(Locus = ~gene.expn) %>%
+#     mutate_(S.condA_mean = ~rollmean(condA_mean, smooth, fill = "extend")) %>%
+#     mutate_(S.condB_mean = ~rollmean(condB_mean, smooth, fill = "extend")) %>%
+#     mutate_(S.diff_mean = ~rollmean(dif_mean, smooth, fill = "extend")) %>%
+#     mutate_(S.condA_SD = ~rollmean(condA_SD, smooth, fill = "extend")) %>%
+#     mutate_(S.condB_SD = ~rollmean(condB_SD, smooth, fill = "extend"))
+#
+#   # Tidy
+#   df.tidy <- df %>% gather_(key_col = "Condition", value_col = "Normalised.Mean",
+#                             gather_cols = c("condA_mean", "condB_mean"))
+#   df.tidy <- df %>% gather_(key_col = "S.Condition", value_col = "S.Normalised.Mean",
+#                             gather_cols = c("S.condA_mean", "S.condB_mean"))
+#   df.tidy
+# }
 
